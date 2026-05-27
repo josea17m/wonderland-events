38e1db4ad87f8958cf7b170689e27987db0432a1
Jose Montero <jamtbarca10@gmail.com>
2026-05-26 22:24:33 -0500

    feat: tarea 7: seo base y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wonderlandevents.mx",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
