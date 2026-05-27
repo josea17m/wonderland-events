1e4ea9450d72c2e04bb034d6d9330747cb8f02be
Jose Montero <jamtbarca10@gmail.com>
2026-05-26 22:44:02 -0500

    fix: tarea 18: preparar config para dominio final

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  // TODO(datos): confirmar dominio final y hosting antes del deploy productivo
   site: "https://wonderlandevents.mx",
 
   vite: {
