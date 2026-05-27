637391a471b62728adcb0804a97e4fa45a954f45
Jose Montero <jamtbarca10@gmail.com>
2026-05-26 22:23:21 -0500

    refactor: tarea 6: layout base compartido

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wonderlandevents.mx",
   vite: {
     plugins: [tailwindcss()],
   },
